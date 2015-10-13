1f12ba6afe7845d903fb022afaa844e0702befc3
RidiQirici <ridi.qirici@imb.al>
2015-10-13 12:30:06 +0200

    Hyrja fillestare
    Plugin per zebren

---
+++ www/zebrushe.js
@@ -0,0 +1,46 @@
+﻿var ZebrusheLoader = function (require, exports, module) {
+    
+    var exec = require('cordova/exec');
+    
+    var Zebrushe = function () {
+	
+    };
+    
+    Zebrushe.prototype.printText = function (text, successCallback, errorCallback) {
+        
+        if (successCallback === null) {
+            successCallback = function (response) {
+                console.log('Zebrushe.printText sukses: ' + response);
+            };
+        }
+        
+        if (errorCallback === null) {
+            errorCallback = function (error) {
+                console.error('Zebrushe.printText deshtim: ' + error);
+            };
+        }
+        
+        if (typeof errorCallback != "function") {
+            console.error("Zebrushe.printText failure: parametri deshtimit nuk eshte funksion");
+            return;
+        }
+        
+        if (typeof successCallback != "function") {
+            console.error("Zebrushe.printText failure: parametri callback i suksesit duhet te jete patjeter funksion");
+            return;
+        }
+        
+        exec(
+            successCallback,
+            errorCallback,
+            "Zebrushe",
+            "printText",
+            [text]
+        );
+    };
+    
+    module.exports = new Zebrushe();
+};
+
+ZebrusheLoader(require, exports, module);
+cordova.define("cordova/plugin/Zebrushe", ZebrusheLoader);
